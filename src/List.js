cd8c7e0b43c4fc0cb3047bdacdc13b5690a70c3e
NandhiniSiva37 <nandhinisiva1237@gmail.com>
2023-11-25 10:58:54 +0530

    list

--- src/List.js
+++ src/List.js
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { IoTrash } from "react-icons/io5";
 
 import React from 'react'
 
@@ -16,18 +17,33 @@ const [items,setItems]=useState([
             {  id:4,
                 checked:true,
                 item:"offers"},])
+const handleCheck=(id)=>{
+const listItems=items.map((item)=>
+item.id===id ? {...item,checked:!item.checked}:item)
+setItems(listItems)}
+
+const handleDelete=(id)=>{
+  const listItems=items.filter((item)=>
+  item.id!==id)
+  setItems(listItems)
+}
 
   return (
     <div>
 <ul>
 {items.map((item) => (
-<li>
+<li className="item" key={item.id}> 
   <input 
   type='checkbox'
+  onChange={()=>handleCheck(item.id)}
   checked={item.checked}
   />
   <label>{item.item}</label>
-  <button>delete</button>
+  <IoTrash
+  role="button" 
+  onClick={()=>handleDelete(item.id)}
+  tabIndex="0"/>
+  
 </li>
 
 ))}
